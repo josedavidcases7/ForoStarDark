faa481e269b0e86db631d5f35cad3418ae7eafc4
Alex <caroazulpavorangers@gmail.com>
2025-04-24 23:49:00 +0200

    css de los comentarios completados tambien

--- src/app/comentarios/comentarios.component.ts
+++ src/app/comentarios/comentarios.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../services/auth.service'; 
 
 @Component({
   selector: 'app-comentarios',
@@ -8,11 +9,13 @@ import { CommonModule } from '@angular/common';
   templateUrl: './comentarios.component.html',
   styleUrls: ['./comentarios.component.scss']
 })
-export class ComentariosComponent {
+export class ComentariosComponent implements OnInit {
   @Input() comentarios: { texto: string, archivo: string | null, fotoUsuario: string }[] = [];
-  comentariosMostrados: { texto: string, archivo: string | null, fotoUsuario: string }[] = [];
+  comentariosMostrados: { texto: string, archivo: string | null, fotoUsuario: string, nombreUsuario: string }[] = []; // Añadido el campo nombreUsuario
   comentariosPorPagina = 10;  // Cantidad de comentarios por cada "página"
   paginaActual = 0;  // Empezamos en la página 0
+
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {
     this.mostrarComentarios();
@@ -23,7 +26,10 @@ export class ComentariosComponent {
     const inicio = this.paginaActual * this.comentariosPorPagina;
     const fin = inicio + this.comentariosPorPagina;
     // Se acumulan los comentarios ya mostrados y los nuevos
-    this.comentariosMostrados = [...this.comentariosMostrados, ...this.comentarios.slice(inicio, fin)];
+    this.comentariosMostrados = [...this.comentariosMostrados, ...this.comentarios.slice(inicio, fin).map(comentario => {
+      const nombreUsuario = this.authService.getUsername() || '';  // Obtener el nombre de usuario desde AuthService
+      return { ...comentario, nombreUsuario }; // Añadir nombreUsuario a cada comentario
+    })];
   }
 
   // Función que se llama cuando el usuario hace clic en "Ver más"
@@ -36,6 +42,4 @@ export class ComentariosComponent {
   hayMasComentarios() {
     return this.paginaActual * this.comentariosPorPagina < this.comentarios.length;
   }
-
-  
 }
